de59543db358f7ee28fa763c5ebe68c6d0e996fd
user <yaz20180596@std.psut.edu.jo>
2023-01-02 19:42:03 +0300

    Initial commit

---
+++ app/src/main/java/com/example/inventorymanager/AddInventory.kt
@@ -0,0 +1,13 @@
+package com.example.inventorymanager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddInventory : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_inventory)
+    }
+
+    fun 
+}
